fd43596e286c72805f00c4b77f0f0c7b73813ea1
Alexander Beshmelnitsyn <alexphisher91@gmail.com>
2020-11-28 12:27:25 +0700

    add openapi-schema

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from products.urls import product_router
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('openapi/', get_schema_view(
+        title="WMS Service",
+        description="API for developers wms service"
+    ), name='wms-schema'),
 
 
 ]
